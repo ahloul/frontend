ca91b304a57ab4f68d7418779ce58f9bf88bd16b
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-05-02 18:23:10 +0200

    Fix: Compare Objects Fix

--- utils/object.js
+++ utils/object.js
@@ -4,17 +4,14 @@ import { transform, isObject, isEqual, isArray } from 'lodash'
  * Deep diff between two object, using lodash
  * @param  {Object} object Object compared
  * @param  {Object} base   Object to compare with
- * @param  {Boolean} base   Ignore arrays if true
  * @return {Object}        Return a new object who represent the diff
  */
-export const difference = (object, base, ignoreArray) => {
+const difference = (object, base) => {
   function changes(object, base) {
     return transform(object, function (result, value, key) {
       if (!isEqual(value, base[key])) {
         result[key] =
-          isObject(value) &&
-          (ignoreArray ? !isArray(value) : true) &&
-          isObject(base[key])
+          isObject(value) && !isArray(value) && isObject(base[key])
             ? changes(value, base[key])
             : value
       }

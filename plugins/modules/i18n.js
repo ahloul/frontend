176a19d98658e915d84c8a8a2f50f1a99e4d6ffe
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-05-03 17:22:49 +0200

    Feat: Initial opening times

--- plugins/modules/i18n.js
+++ plugins/modules/i18n.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
-import de from '~/lang/de-DE.js'
-import en from '~/lang/en-US.js'
+import de from '~/lang/de-DE.json'
+import en from '~/lang/en-US.json'
 
 Vue.use(VueI18n)
 

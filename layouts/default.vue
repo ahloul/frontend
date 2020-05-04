69b1898d03cadc66692db44dadd070169136dca2
Lukas Mauser <l.mauser.dev@gmail.com>
2020-05-04 15:37:56 +0200

    Fix: i18n 'Okay' warning

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <toast />
+
     <modal
       :show="showModal"
       :dismiss="null"
-      confirm="Okay"
+      confirm="confirm"
       centered
       @confirm="hideModal"
       >{{ $t(message) }}</modal
     >
+
     <navigation
       class="hidden md:block sticky top-0 py-2 container mx-auto px-2"
     />
+
     <nuxt class="container flex-1 mx-auto px-2 mb-20 md:mb-10" />
     <navbar class="block md:hidden fixed bottom-0 w-full" />
     <footer-bar class="px-8 pt-10 pb-20 md:pb-10" />
